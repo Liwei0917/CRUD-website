7fe0708dc00c12c8f600fa11380c88e8ed67cf80
Liwei0917 <liwei09070@outlook.com>
2026-07-07 12:13:45 +0900

    Update client.js

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -4,7 +4,7 @@ const baseURL = import.meta.env.VITE_API_BASE_URL
   ? `${import.meta.env.VITE_API_BASE_URL.replace(/\/$/, '')}/api`
   : '/api';
 
-const api = axios.create({ https://curdwebsitesim.netlify.app/api });
+const api = axios.create({ https://curdwebsitesimp.netlify.app });
 
 export const TOKEN_KEY = 'crud_token';
 
